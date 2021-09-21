736dd4dc1a604b725d28475a7b0cc9875efb6b9f
Harsh <harsh.here17@gmail.com>
2021-09-22 03:13:35 +0530

    update chat-dummy data

--- lib/models/chatData.dart
+++ lib/models/chatData.dart
@@ -9,4 +9,14 @@ final List<BotChat> chatdata = [
   BotChat(message: 'heyaa', sid: 'bot', rid: 'usr'),
   BotChat(message: 'hello', rid: 'bot', sid: 'usr'),
   BotChat(message: 'heyaa', sid: 'bot', rid: 'usr'),
+  BotChat(
+      message: 'Following song found over these platforms:',
+      sid: 'bot',
+      rid: 'usr',
+      spotifyUrl: 'a',
+      appleMusicUrl: 'a',
+      coverImgUrl: 'https://picsum.photos/200',
+      gaanaUrl: 'a',
+      wynkUrl: 'a',
+      ytMusicUrl: 'a'),
 ];
