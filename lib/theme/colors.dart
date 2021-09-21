0eb50b376824c9e5043dc947e735134d363e1b2f
Harsh <harsh.here17@gmail.com>
2021-09-22 03:18:05 +0530

    logo Colors define

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
 final Color primaryColor = Colors.blue;
-final Color white = Colors.white70;
+final Color white = Colors.white;
 final Color accent = Colors.grey;
+final Color spotifyDefault = Color(0xFF1DB954);
+
+final LinearGradient lg = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment(0.8, 0.0), // 10% of the width, so there are ten blinds.
+  colors: <Color>[
+    Color(0xffffffff),
+    Color(0xfffa57c1),
+    Color(0xffb166cc),
+    Color(0xff7572ff),
+    Color(0xff69a6f9)
+  ], // red to yellow // repeats the gradient over the canvas
+);
+
+final Color ytMusicDefault = Color(0xffc3352e);
+final Color gaanaDefault = Colors.orange.shade900;
+final Color wynkDefault = Colors.red;
