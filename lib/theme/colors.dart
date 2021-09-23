42537f58c6d0a01e6c24b50c79d6c2e905c43571
Harsh <harsh.here17@gmail.com>
2021-09-23 19:22:00 +0530

    colors update

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
-final Color primaryColor = Colors.blue;
-final Color white = Colors.white;
-final Color accent = Colors.grey;
+final Color lightPrimaryColor = Color(0xff3CD3AD);
+final Color darkPrimaryColor = Color(0xff3CD3AD);
+final Color lightBgColor = Colors.white;
+final Color darkBgColor = Colors.black87;
+final Color lightAccent = Color(0xff3CD3AD).withOpacity(0.3);
+final Color darkAccent = Color(0xff3CD3AD).withOpacity(0.7);
 final Color spotifyDefault = Color(0xFF1DB954);
 
 final LinearGradient lg = LinearGradient(
@@ -14,6 +17,15 @@ final LinearGradient lg = LinearGradient(
     Color(0xffb166cc),
     Color(0xff7572ff),
     Color(0xff69a6f9)
+  ], // red to yellow // repeats the gradient over the canvas
+);
+
+final LinearGradient lg1 = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment(0.8, 0.0), // 10% of the width, so there are ten blinds.
+  colors: <Color>[
+    Color(0xff4CB8C4),
+    Color(0xff3CD3AD),
   ], // red to yellow // repeats the gradient over the canvas
 );
 
