9c4bca28ed1ecd3a199d5b2acbcd8a07e045e3e9
Harsh <harsh.here17@gmail.com>
2021-09-26 23:35:04 +0530

    model update

--- frontend/lib/models/chat.model.dart
+++ frontend/lib/models/chat.model.dart
@@ -1,27 +1,25 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-
 class BotChat {
   final String message;
   String sid;
   String rid;
-  String spotifyUrl;
-  String gaanaUrl;
-  String wynkUrl;
-  String appleMusicUrl;
-  String ytMusicUrl;
-  String coverImgUrl;
+  String? spotifyUrl;
+  String? gaanaUrl;
+  String? wynkUrl;
+  String? appleMusicUrl;
+  String? ytMusicUrl;
+  String? coverImgUrl;
 
-  BotChat(
-      {required this.message,
-      this.rid = 'bid',
-      this.sid = 'usr',
-      this.coverImgUrl = '',
-      this.spotifyUrl = '',
-      this.appleMusicUrl = '',
-      this.gaanaUrl = '',
-      this.wynkUrl = '',
-      this.ytMusicUrl = ''});
+  BotChat({
+    required this.message,
+    this.rid = 'bid',
+    this.sid = 'usr',
+    this.coverImgUrl,
+    this.spotifyUrl,
+    this.appleMusicUrl,
+    this.gaanaUrl,
+    this.wynkUrl,
+    this.ytMusicUrl,
+  });
 
   factory BotChat.fromJson(Map<String, dynamic> json) {
     return BotChat(message: json['message']);
