cdc96154ed8b654110d1314c37d9fd0cf35fb843
Harsh <harsh.here17@gmail.com>
2021-09-26 23:40:03 +0530

    (fix): api calling

--- frontend/lib/services/bot.service.dart
+++ frontend/lib/services/bot.service.dart
@@ -3,6 +3,6 @@ import 'package:soundconverge/services/endpointConfig.dart';
 
 Future<BotChat> querySong(String song) async {
   final url = finalUrl(Endpoint.querySong);
-  final resp = await httpClient(url, Request.post);
+  final resp = await httpClient(url, Request.post, body: {'message': song});
   return BotChat.fromJson(resp);
 }
